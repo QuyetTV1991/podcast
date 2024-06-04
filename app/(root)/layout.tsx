5627628787d0672d115c964dcf92b879a5b501b5
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-06-04 10:48:21 +0000

    leftsidebar & structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div>
+      <main>
+        <LeftSidebar />
+        {children}
+        <p className="text-white-1">RIGHT SIDEBAR</p>
+      </main>
+    </div>
+  );
+}
