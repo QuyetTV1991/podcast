8f38b9559a7ba10862a8ae888b393d4fb0920859
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-06-05 10:52:22 +0000

    setup clerk and convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://epic-halibut-23.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
