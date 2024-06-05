02223b1f4b97b9cb0a91e2a81df53a5f32cf337e
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-06-05 02:52:37 +0000

    homepage layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,36 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
+import Image from "next/image";
 import React from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div>
-      <main>
+    <div className="flex flex-col relative">
+      <main className="relative flex bg-black-3">
         <LeftSidebar />
-        {children}
-        <p className="text-white-1">RIGHT SIDEBAR</p>
+
+        <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">
+              <Image
+                src="/icons/logo.svg"
+                alt="menu logo"
+                width={30}
+                height={30}
+              />
+              <MobileNav />
+            </div>
+            <div className="flex flex-col md:p-14">
+              {/* Toaster */}
+              {children}
+            </div>
+          </div>
+        </section>
+
+        <RightSidebar />
       </main>
     </div>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+import React from "react";
 
 const Home = () => {
   return (
-    <div className='mt-9 flex flex-col gap-9'>
-      <section className='flex flex-col gap-5'>
-        <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+    <div className="mt-9 flex flex-col gap-9">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ imgURL, id, title, description }) => (
+            <PodcastCard
+              key={id}
+              podcastId={id}
+              title={title}
+              imgURL={imgURL}
+              description={description}
+            />
+          ))}
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
