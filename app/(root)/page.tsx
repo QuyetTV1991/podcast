68dbd92622a0231822fcd4a307f2af6292ca5c25
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-06-06 07:59:12 +0000

    remove tasks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,12 +2,9 @@
 
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
 import React from "react";
 
 const Home = () => {
-  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
