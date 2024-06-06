abe3aa4adb04d71355845a39852b9f3e6791dde9
Tran Van Quyet <55977448+QuyetTV1991@users.noreply.github.com>
2024-06-06 07:51:45 +0000

    clerk and convex

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <main>{children}</main>;
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src="/images/bg-img.png"
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      {children}
+    </main>
+  );
 }
